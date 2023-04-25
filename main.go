a16deadebed163eab0d74bdf0780c47289edcf31
JIeJaitt <498938874@qq.com>
2023-04-25 15:06:12 +0800

    feat: database.InitMysql() && routers.InitRouter()

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package blogweb_gin
+package main
+
+import (
+	"blogweb-gin/database"
+	"blogweb-gin/routers"
+)
+
+func main() {
+	database.InitMysql()
+	router := routers.InitRouter()
+	//静态资源
+	router.Static("/static", "./static")
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
+}
