a0f0cb028b11b4e569856df38caf03389a7c1e73
JIeJaitt <498938874@qq.com>
2023-04-25 18:15:41 +0800

    style:    代码格式（不影响功能，例如空格、分号等格式修正）

--- routers/router.go
+++ routers/router.go
@@ -9,6 +9,7 @@ func InitRouter() *gin.Engine {
 
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*")
+
 	// 注册：
 	router.GET("/register", controllers.RegisterGet)
 	router.POST("/register", controllers.RegisterPost)
